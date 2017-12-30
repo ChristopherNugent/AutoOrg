f8ec99db586b9d293cd4a3dc03902c56bcf5a4c1
Christopher Nugent <awedeven+github@gmail.com>
2017-12-30 14:28:38 -0800

    Added optional argument for overwriting files.

--- file-organizer.py
+++ file-organizer.py
@@ -4,6 +4,9 @@ from shutil import move
 from datetime import datetime
 from sys import argv
 import re
+import argparse
+
+
 
 
 def list_files(tdir):
@@ -17,20 +20,23 @@ def filter_files(pattern, file_names):
     return [p.match(f) for f in file_names if p.match(f) is not None]
 
 
-def move_file(filename, src, dst):
+def move_file(filename, src, dst, overwrite):
     """A file move, of filename in src to dst, creating dst if needed"""
     makedirs(dst, exist_ok=True)
-    if not isfile(join(dst, filename)):
+    if overwrite or not isfile(join(dst, filename)):
         print('Moving ' + filename + ' from ' + src + ' to ' + dst)
         move(join(src, filename), join(dst, filename))
     else:
-        print(join(dst, filename) + " already exists, not moving to avoid overwrite...")
+        print(join(dst, filename) +
+              " already exists, not moving to avoid overwrite...")
 
 
-def main(pattern):
+def main(pattern, overwrite):
     now = datetime.now()
     print(now.strftime(
         "%d-%m-%Y %H:%M:%S | Running file organizer with pattern " + pattern))
+    if overwrite:
+        print("Overwrite set to True")
     src = expanduser('~') + '/Documents/'
     file_names = list_files(src)
     filtered_matches = filter_files(pattern, file_names)
@@ -41,13 +47,18 @@ def main(pattern):
         print("No matching files found.")
     for f in filtered_matches:
         dst = join(src, f.group().lower())
-
-        move_file(f.string, src, dst)
+        move_file(f.string, src, dst, overwrite)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pattern", help="regex to match files to move")
+    parser.add_argument("-o", "--overwrite",
+                        help="overwrite existing files", action="store_true")
+    args = parser.parse_args()
     try:
-        pattern = argv[1]
-        main(pattern)
+        pattern = args.pattern
+        overwrite = args.overwrite
+        main(pattern, overwrite)
     except IndexError:
         print('Error: Pattern not specified. Exiting...')
