37d3085ea363e41e8bba07f5565796917ece78e4
Christopher Nugent <awedeven+github@gmail.com>
2017-12-30 13:05:00 -0800

    Switch to os.path.join() from string concatenation for paths.

--- file-organizer.py
+++ file-organizer.py
@@ -1,5 +1,5 @@
 from os import listdir, makedirs
-from os.path import isfile, expanduser
+from os.path import isfile, expanduser, join
 from shutil import move
 from datetime import datetime
 import re
@@ -19,31 +19,30 @@ def filter_files(pattern, file_names):
 def move_file(filename, src, dst):
     """A file move, of filename in src to dst, creating dst if needed"""
     makedirs(dst, exist_ok=True)
-    move(src + filename, dst + filename)
+    move(join(src, filename), join(dst, filename))
 
 
-def main():
+def main(pattern):
     now = datetime.now()
     print(now.strftime("%d-%m-%Y %H:%M:%S | Running file organizer..."))
-    target_dir = expanduser('~') + '/Documents/'
-    file_names = list_files(target_dir)
-    filtered_matches = filter_files('^\w{3}\d{3}', file_names)
+    src = expanduser('~') + '/Documents/'
+    file_names = list_files(src)
+    filtered_matches = filter_files(pattern, file_names)
     if filtered_matches:
-        print("Found files: " + ', '.join([m.string for m in filtered_matches]))
+        print("Found files: " +
+              ', '.join([m.string for m in filtered_matches]))
     else:
         print("No matching files found.")
     for f in filtered_matches:
+        dst = join(src, f.group().lower())
         print(
             'Moving ' +
             f.string +
             ' from ' +
-            target_dir +
-            ' to ' +
-            target_dir +
-            f.group() +
-            '/')
-        move_file(f.string, target_dir, target_dir + f.group().lower() + '/')
+            src +
+            ' to ' + dst)
+        move_file(f.string, src, dst)
 
 
 if __name__ == '__main__':
-    main()
+    main('^\w{3}\d{3}')
